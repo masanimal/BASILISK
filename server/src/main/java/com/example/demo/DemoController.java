20172ad4416ea7cdd67c5ecb04f050cec65db2c3
Masataka Ohta <ohtamasataka@Masa-MacBook-Air.local>
2023-12-17 17:07:43 +0900

    修正

--- server/src/main/java/com/example/demo/DemoController.java
+++ server/src/main/java/com/example/demo/DemoController.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class DemoController {
     @Autowired
     DemoService demoService;
